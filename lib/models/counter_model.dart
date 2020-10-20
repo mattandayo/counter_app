fef55614c8068e7c7022a43fc0d30aa0f3a19cdb
mattandayo <mattandayo@gmail.com>
2020-10-20 23:46:31 +0900

    Add counter model

---
+++ lib/models/counter_model.dart
@@ -0,0 +1,25 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class CounterModel extends Model {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+
+    notifyListeners();
+  }
+
+  void decrement() {
+    _counter--;
+
+    notifyListeners();
+  }
+
+  void reset() {
+    _counter = 0;
+
+    notifyListeners();
+  }
+}
